c47f25f11851ed69eb73d5b0f326e4de005ff93a
mark-50-spec <soumya260503@gmail.com>
2025-04-02 15:19:18 +0530

    Add files via upload

---
+++ Day 6/Factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num, factorial = 1, i;
+    printf("Enter a non-negative : ");
+    scanf("%d", &num);
+    
+        for (i = 1; i <= num; i++)
+		 {
+            factorial = factorial * i;
+        }
+        printf("Factorial of %d = %d\n", num, factorial); 
+    
+
+    return 0;
+}
