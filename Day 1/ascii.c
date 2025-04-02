1433614de6f659a6aee1b04b1c484a5096b31735
mark-50-spec <soumya260503@gmail.com>
2025-04-02 09:42:56 +0530

    Add files via upload

---
+++ Day 1/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    printf("Enter the character: ");
+    scanf("%c", &a);
+
+    printf("%d\n", a);
+
+    return 0;
+}
---
+++ Day 1/subtraction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+  int a = 50, b = 20;
+  int c = a - b;
+  scanf("%d", &a);
+  printf( "The value of the subtraction is\n");
+  printf("%d %d %d\n", a, b, c);
+  return 0;
+}
