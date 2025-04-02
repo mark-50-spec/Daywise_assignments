ad9accfd08a5fd35954e2b8fbba88fc1d7849e83
mark-50-spec <soumya260503@gmail.com>
2025-04-02 15:02:50 +0530

    Add files via upload

---
+++ Day 4/odd_even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number= ");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("It is an even no. ");
+	else
+	printf("It is odd ");
+	return 0;
+}
